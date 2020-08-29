0b75fcef0bff144896fa7511aad14d2fa7fc60d5
AbdulMalik <abdumaliksharipov@gmail.com>
2020-08-29 21:59:57 +0500

    Some changes

--- weather-vanilla/app.js
+++ weather-vanilla/app.js
@@ -2,8 +2,30 @@
 
 const api={
     key:"b6cf9d3ea1693c4c05ed173fa6233f84",
-    baseurl:"api.openweathermap.org/data/2.5"
+    base:"api.openweathermap.org/data/2.5"
 }
 
 const searchBox=document.querySelector(".search-box");
 searchBox.addEventListener("keypress",setQuery);
+
+
+function setQuery(e){
+    if(e.keyCode==13){
+        getResults(searchBox.value);
+        console.log(searchBox.value);
+    }
+}
+
+
+function getResults(query){
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather=>{
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults(weather){
+    console.log(weather);
+    let city=document.querySelector(".location .city");
+    city.innerText=`${weather.name}, ${weather.sys.country}`;
+}
